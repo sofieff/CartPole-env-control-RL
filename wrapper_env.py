7580da88d78ba44bab7025f019b647aacca3d781
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-08-31 15:22:44 +0200

    adjustments to wrapper env

--- wrapper_env.py
+++ wrapper_env.py
@@ -1,31 +1,37 @@
 import gymnasium as gym
 import numpy as np
+from gymnasium.envs.classic_control.cartpole import CartPoleEnv
+class LearnedCartPoleEnv(CartPoleEnv):
 
-class WrapperEnv(gym.Env):
+  def __init__(self):
+    super().__init__(render_mode=None)
 
-  def __init__(self, env_name="CartPole-v1", neural_network=None):
-    breakpoint()
-    self.env = gym.make(env_name, render_mode="rgb_array")
-    self.neural_network = neural_network
-    self.state = None
+  def neural_network(self, state, action):
+    return np.array([0.0, 0.0, 0.0, 0.0])
 
-  def reset(self, seed=None, options=None):
+
+  def reset(self,*, seed: int | None = None, options: dict | None = None,):
     self.state = np.array([np.random.uniform(-4.8, 4.8), np.random.uniform(-100, 100),np.random.uniform(-0.418, 0.418), np.random.uniform(-100, 100) ])
     return self.state, {}
 
-  def step(self, action: np.ndarray):
+  def step(self, action: int):
     reward = 1
-    trucated = False
-    terminated = False
-
     next_state = self.neural_network(self.state, action)  # HERE GOES A NeURAL NETWORK THAT TAKES IN STATE + ACTION AND GIVES US NEW STATE
+    x, _, theta, _ = next_state
+
+    terminated = bool(
+            x < -self.x_threshold
+            or x > self.x_threshold
+            or theta < -self.theta_threshold_radians
+            or theta > self.theta_threshold_radians
+        )
+
     self.state = next_state
 
-    return next_state, reward, trucated, terminated, {}
+    return next_state, reward, terminated, False, {}
 
 
-
-
-
-
-WrapperEnv()
+env = LearnedCartPoleEnv()
+print(env)
+print(env.reset())
+print(env.step(1))
